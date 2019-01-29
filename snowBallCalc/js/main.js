e09e7b3647a9675241afc01ab85aeb14953f95d3
Baidis <38296360+Baidis@users.noreply.github.com>
2019-01-29 12:51:42 -0500

    Moved JS to main.js
    Moved JS to main.js 
    Code not functional

---
+++ snowBallCalc/js/main.js
@@ -0,0 +1,34 @@
+/*   <script type="text/javascript">
+    const dItem = new Debt("dItem",0, 0, 0);
+    
+
+
+      function Debt(name, balence, payment, rate, months) {
+        this.name = name;
+        this.balence = balence;
+        this.payment = payment;
+        this.rate = rate;
+        this.months = months;
+      }
+
+
+
+      function debtLoop(u,i,j,k,l){
+        for (l = 0; 0 < i; l++) {
+          if (l == 0){
+              document.write(i+"<br>");
+              l++;
+              }
+         else {
+              i = (i-j)*(k/12+1);
+              document.write(`Left on ${u} loan ${i}<br>
+                              ${u} payments made ${(l-1)}<br>`);
+              }
+        }
+
+              }
+
+      debtLoop(dItem.name,dItem.balence, dItem.payment, dItem.rate, dItem.months)
+
+
+    </script> */
